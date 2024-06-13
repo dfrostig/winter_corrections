b9b298654e545591dd15866cc483ec6ee58ff3a0
Robert Stein <rdstein@caltech.edu>
2024-06-13 16:00:22 -0700

    Use pathlib for relative paths

--- winternlc/config.py
+++ winternlc/config.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
+code_dir = Path(__file__).parent
+
+data_dir = code_dir / "data"
+
 # paths
-fits_file = "../data/example_data/example_science_image_mef.fits"
-save_dir = "../data/example_data/"
+fits_file = data_dir / "example_data/example_science_image_mef.fits"
+save_dir = data_dir / "example_data"
 cor_dir = "/home/winter/GIT/winter_linearity/data/linearity_corrections"
 test_directory = "/data/flats_iwr/20240610"
-output_directory = "../data/linearity_corrections"
+output_directory = data_dir / "linearity_corrections"
 
 # variables
 cutoff = 56000
